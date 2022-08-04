64d0c0b30cbff2ccf9748c69b5c812414b3baf66
BaoSkyFall <39408162+BaoSkyFall@users.noreply.github.com>
2022-08-05 00:39:58 +0700

    add customize theme

--- FE/extra-webpack.config.js
+++ FE/extra-webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         options: {
           modifyVars: { // modify theme variable
             'primary-color': '#20818C',
+            'link-color': '#20818C',
             'border-radius-base': '2px'
           },
           javascriptEnabled: true
